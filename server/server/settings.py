447f2037dc371112b70250ba03c0ad19434e893f
Savir <hector.blanco.rivas@gmail.com>
2018-02-03 20:49:43 -0500

    add hector-blanco.herokuapp.com to ALLOWED_HOSTS

--- server/server/settings.py
+++ server/server/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'y=ca-c9qo4m73^bq#n@opp#ic@&%&9_eb_d$^zfm+&9*x#5#t_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'hector-blanco.herokuapp.com']
 
 
 # Application definition
